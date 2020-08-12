9d43a75ad314238359ca16fbae5897efb60ecbf6
aghe73 <rozsa.agnes.cv@gmail.com>
2020-08-12 14:25:05 +0200

    Sprint 9 kész

---
+++ js/main.js
@@ -0,0 +1,55 @@
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber
+
+    showSumPrice(price, amountNumber);
+}
+
+function showSumPrice(price, amountNumber) {
+    let showAmount = document.querySelector("span.show-amount");
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhat");
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 terméket kell vásárolnia!");
+    } else {
+        let amount = amountNumber * price;
+        showAmount.innerHTML = amount;
+    }
+}
+
+// Helptext hozzáadása
+
+let helpText = document.createElement("small");
+helpText.classname = "form-text text-muted";
+helpText.innerHTML = "Adja meg a feltéteket!"
+
+let parent = document.querySelector("div.form-group:nth-child(1)");
+parent.appendChild(helpText)
+
+let alertCloseButtons = document.querySelectorAll(".close[data-dismiss='alert']");
+let alertCloseEventHandlerFunction = function (ev) {
+    this.parentElement.style.display = "none";
+};
+for (let i = 0; i < alertCloseButtons.length; i++) {
+    alertCloseButtons[i].addEventListener("click", alertCloseEventHandlerFunction);
+}
+
+//Select elem kitöltése
+let toppings = [
+    "Szalonna",
+    "Hagyma",
+    "Tükörtojás",
+    "Libamáj",
+    "Extra sonka"
+];
+let toppingSelect = document.querySelector("#topInput");
+let index = 0;
+while(index < toppings.length) {
+    let option = document.createElement("option");
+    option.value = index;
+    option.innerHTML = toppings[index];
+    toppingSelect.appendChild(option);
+    index++;
+}
